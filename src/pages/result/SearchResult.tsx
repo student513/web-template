cbba85fc53b6bcd40cb9f5cee26741cb7e179370
hjjo <compa513@naver.com>
2023-02-18 22:32:15 +0900

    add thumbnail, favicon

--- src/pages/result/SearchResult.tsx
+++ src/pages/result/SearchResult.tsx
@@ -1,10 +1,12 @@
 import Back from "@/assets/Back.svg";
+import DefaultFavicon from "@/assets/default_favi.svg";
+import DefaultThumbnail from "@/assets/default_thumb.svg";
 import { FallbackSkeleton } from "@/components/FallbackSkeleton";
 import { SearchInput } from "@/components/Input";
 import { useGetSearchResults } from "@/providers/ApiProvider";
 import { styled } from "@stitches/react";
 import { useQuery } from "@tanstack/react-query";
-import Image from "next/image";
+import Image, { ImageLoaderProps } from "next/image";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 import { SearchResultType } from "../api/getSearchResult";
@@ -71,18 +73,49 @@ const SearchResultItem = ({
 }: {
   searchResult: SearchResultType["documents"][number];
 }) => {
+  const [isError, setIsError] = useState(false);
   return (
     <SearchResultItemContainer>
       <SearchResultItemThumbnail>
-        {/* <Image
-          src={searchResult.url}
-          alt={DefaultThumbnail}
-          width={72}
-          height={72}
-        /> */}
+        {isError ? (
+          <Image src={DefaultThumbnail} alt={"thumbnail"} />
+        ) : (
+          <Image
+            src={searchResult.url}
+            alt={"thumbnail"}
+            loader={() =>
+              imageLoader({ src: searchResult.imageUrl, width: 72 })
+            }
+            width={72}
+            height={72}
+            onError={() => setIsError(true)}
+          />
+        )}
       </SearchResultItemThumbnail>
+      <SearchResultItemSubContainer>
+        <SearchResultItemTitle>{searchResult.title}</SearchResultItemTitle>
+        <SearchResultItemContent>
+          {searchResult.faviconUrl ? (
+            <Image
+              src={searchResult.faviconUrl}
+              alt="favicon"
+              width={14}
+              height={14}
+              loader={() =>
+                imageLoader({ src: searchResult.faviconUrl, width: 14 })
+              }
+            />
+          ) : (
+            <Image src={DefaultFavicon} alt={"favicon"} />
+          )}
+        </SearchResultItemContent>
+      </SearchResultItemSubContainer>
     </SearchResultItemContainer>
   );
+};
+
+const imageLoader = (props: ImageLoaderProps) => {
+  return `${props.src}`;
 };
 
 const SearchResultItemContainer = styled("div", {
@@ -106,14 +139,12 @@ const SearchResultItemThumbnail = styled("div", {
 const SearchResultItemTitle = styled("div", {
   width: 457,
   height: 20,
-  backgroundColor: "#F2F3F7",
   borderRadius: 4,
   marginTop: 19,
 });
 const SearchResultItemContent = styled("div", {
   width: 160,
   height: 14,
-  backgroundColor: "#F2F3F7",
   borderRadius: 4,
   marginTop: 31,
   marginBottom: 20,

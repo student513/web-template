931b87261fd5c298366799b4176a418a86a76acd
hjjo <compa513@naver.com>
2023-02-19 22:39:50 +0900

    마무리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { AxiosInstanceProvider } from "@/providers/AxiosInstanceProvider";
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
@@ -21,7 +21,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <AxiosInstanceProvider>
           <ApiProvider>
-            <Component {...pageProps} />
+            <Suspense fallback={<>loading...</>}>
+              <Component {...pageProps} />
+            </Suspense>
           </ApiProvider>
         </AxiosInstanceProvider>
       </QueryClientProvider>
